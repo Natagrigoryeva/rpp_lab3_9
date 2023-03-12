5154f0855439d78f0da8d03ef61116d2f69f9484
Natagrigoryeva <114040558+Natagrigoryeva@users.noreply.github.com>
2023-03-12 17:05:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,58 +1,71 @@
 import http.client
 import json
 
+def operate(data: str, left: int):
 
-# Задание 1. Ответ: {"number":56}, Первое число: 56
+    dict = json.loads(data)
+
+    if dict['operation'] == 'mul':
+
+        return left * dict['number']
+
+    if dict['operation'] == 'sub':
+
+        return left - dict['number']
+
+    if dict['operation'] == 'div':
+
+        return left / dict['number']
+
+    if dict['operation'] == 'sum':
+
+        return left + dict['number']
+
+#Задание 1: Отправить HTTP запрос GET /number/{Вариант}.
+
 connect = http.client.HTTPConnection("167.172.172.227:8000")
-connect.request("GET", "/number/9")
+connect.request ("GET", "/number/9")
 response1 = connect.getresponse()
+print(response1.status, response1.reason)
 decoded_body = response1.read().decode()
-print(decoded_body)
-json_decoded_body = json.loads(decoded_body)
-first = json_decoded_body["number"]
-print('Первое число:', first)
+left = json.loads(decoded_body)['number']
+print(left)
 
-# Задание 2. Ответ:{"operation":"mul","number":17}, Второе число: 17, Умножение: 952
-connect.request("GET", "/number/?option=9")
-response2 = connect.getresponse()
-decoded_body_1 = response2.read().decode()
-print(decoded_body_1)
-json_decoded_body_1 = json.loads(decoded_body_1)
-second = json_decoded_body_1["number"]
-print('Второе число:', second)
-print('Умножение:', int(first * second))
+#Задание 2: Отправить HTTP запрос GET /number/ с параметром запроса option={Вариант}.
 
-# Задание 3. Ответ: {"operation":"sub","number":82}, Третье число:  82, Вычитание: 65
+connect = http.client.HTTPConnection("167.172.172.227:8000")
+connect.request('GET', '/number/?option=9')
+response1 = connect.getresponse()
+print(response1.status, response1.reason)
+decoded_body = response1.read().decode()
+left = operate(decoded_body, left)
+print(left)
+
+#Задание 3: Отправить HTTP запрос POST /number/ с телом option={Вариант}.
+connect = http.client.HTTPConnection("167.172.172.227:8000")
 headers = {'Content-type': 'application/x-www-form-urlencoded'}
-connect.request("POST", "/number/", "option=9", headers)
-response3 = connect.getresponse()
-decoded_body_2 = response3.read().decode()
-print(decoded_body_2)
-json_decoded_body_2 = json.loads(decoded_body_2)
-third = json_decoded_body_2["number"]
-print('Третье число: ', third)
-print('Вычитание:', int(third - second))
+connect.request('POST', '/number/', 'option=9', headers)
+response1 = connect.getresponse()
+print(response1.status, response1.reason)
+decoded_body = response1.read().decode()
+left = operate(decoded_body, left)
+print(left)
 
-# Задание 4. Ответ: {"operation":"div","number":29}, Четвертое число:  29, Деление:  2
+#Задание 4: Отправить HTTP запрос PUT /number/ с телом JSON {"option": {Вариант}}.
+connect = http.client.HTTPConnection("167.172.172.227:8000")
 headers = {'Content-type': 'application/json'}
-body = {'option': 9}
-connect.request("PUT", "/number/", json.dumps(body), headers)
-response4 = connect.getresponse()
-decoded_body_3 = response4.read().decode()
-print(decoded_body_3)
-json_decoded_body_3 = json.loads(decoded_body_3)
-fourth = json_decoded_body_3["number"]
-print('Четвертое число: ', fourth)
-print('Деление: ', int(third / fourth))
+connect.request('PUT', '/number/', json.dumps({'option': 9}), headers)
+response1 = connect.getresponse()
+print(response1.status, response1.reason)
+decoded_body = response1.read().decode()
+left = operate(decoded_body, left)
+print(left)
 
-# Задание 5. Ответ: {"operation":"sum","number":7}, Пятое число:  7, Сумма:  36
-headers = {'Content-type': 'application/json'}
-body = {'option': 9}
-connect.request("DELETE", "/number/", json.dumps(body), headers)
-response5 = connect.getresponse()
-decoded_body_4 = response5.read().decode()
-print(decoded_body_4)
-json_decoded_body_4 = json.loads(decoded_body_4)
-fifth = json_decoded_body_4["number"]
-print('Пятое число: ', fifth)
-print('Сумма: ', int(fourth + fifth))
+#Задание 5: Отправить HTTP запрос DELETE /number/ с телом JSON {"option": {Вариант}}.
+connect = http.client.HTTPConnection("167.172.172.227:8000")
+connect.request('DELETE', '/number/', json.dumps({'option': 9}))
+response1 = connect.getresponse()
+print(response1.status, response1.reason)
+decoded_body = response1.read().decode()
+left = operate(decoded_body, left)
+print(left)
